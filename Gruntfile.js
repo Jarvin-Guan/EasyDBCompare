75749a678b191a87b5fbcc488c1528d5168a1346
Jarvin <jarvin_g@126.com>
2016-01-17 14:14:04 +0800

    add grunt auto build

---
+++ Gruntfile.js
@@ -0,0 +1,23 @@
+module.exports = function (grunt) {
+
+    grunt.initConfig({
+        nodewebkit: {
+            options: {
+                platforms: ['win', 'osx'],
+                buildDir: './build',
+                version: '0.12.1'
+            },
+            src: [
+                'index.html',
+                'package.json',
+                './public/**/*',
+                './node_module/**/*'
+            ]
+        }
+    });
+
+    grunt.loadNpmTasks('grunt-node-webkit-builder');
+
+    grunt.registerTask('build', ['nodewebkit']);
+
+};
